8a6745fc8b15d9bc9cee4d0a25c0e7d62cb8aeda
mesandeepmehta <119277325+mesandeepmehta@users.noreply.github.com>
2023-03-01 14:09:06 +0530

    first commit

---
+++ a.py
@@ -0,0 +1,8 @@
+a=["adam","eve","todd","murphy"]
+b=len(a)
+print(b)
+a.append("rahul")
+print(a)
+z="hello sir how are you, I am pleased to meet you in person here"
+v=z.split()
+w=v.slice(4,6)
---
+++ bubblesort.py
@@ -0,0 +1,13 @@
+#bubblesort it is a technique to sort an array on numbers;
+def bubblesort(a):
+  l=len(a)
+  for i in range(l-1):
+    for j in range(1,l-i-1):
+      if a[j]>a[j+1]:
+        a[j],a[j+1]=a[j+1],a[j]
+      else:
+        pass
+  return(a)
+b=[2,45,42,85,3,6,5,9,2,2,47,5,21,45,654]
+c=bubblesort(b)
+print(c)  
